20bef40b20e0133b8d3bffee206b02f1d64e1fc3
Unknown <mchonaker@gmail.com>
2018-12-19 18:42:57 -0500

    Updated to allow tracking while not projecting as well as more refactor
    Made it so that instead of stopping tracking when turn off projection it just changes texture to a pure black texture. The slowest part of the kinect is picking up people so this allows the kinect to always be tracking and be able to turn on in an instant. 
    
    Also started renaming variables and cleaning up some of the unneeded commented code.

--- Assets/Face/Scripts/ChangeCameraLocation.cs
+++ Assets/Face/Scripts/ChangeCameraLocation.cs
@@ -1,58 +1,81 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeCameraLocation : MonoBehaviour {
+    //public variables for change
+    public float positionOffset;
+    public float rotationOffset;
+    public UnityEngine.UI.Text cameraText;
 
-	public float positionOffset;
-	public float rotationOffset;
-	private Vector3 offsetx;
-	private Vector3 offsety;
-	private Vector3 offsetz;
-	
-	// Use this for initialization
-	void Start () {
-		offsetx = new Vector3(positionOffset,0,0);
-		offsety = new Vector3(0,positionOffset,0);
-		offsetz = new Vector3(0,0,positionOffset);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey("a"))
+    //offset variables
+    private Vector3 offsetx;
+    private Vector3 offsetz;
+    //y direction does not affect kinect in testing
+
+        // Use this for initialization
+    void Start () {
+        offsetx = new Vector3(positionOffset,0,0);
+        offsetz = new Vector3(0,0,positionOffset);
+        cameraText.text = transform.position.ToString();
+    }
+        
+    // Update is called once per frame
+    void Update () {
+        //if text needs to be updated
+        bool update = false;
+        
+        //listen for key inputs        
+        if (Input.GetKey("a"))
         {
-        	transform.position = transform.position+offsetx;
+            transform.position = transform.position+offsetx;
+            update = true;
         }
 
         if (Input.GetKey("d"))
         {
-        	transform.position = transform.position-offsetx;
+            transform.position = transform.position-offsetx;
+            update = true;
         }
         if (Input.GetKey("w"))
         {
-        	transform.position = transform.position+offsetz;
+            transform.position = transform.position+offsetz;
+            update = true;
         }
 
         if (Input.GetKey("s"))
         {
-        	transform.position = transform.position-offsetz;
+            transform.position = transform.position-offsetz;
+            update = true;
         }
         if (Input.GetKey("z"))
         {
-        	transform.position = transform.position+offsety;
+            transform.Rotate(0, rotationOffset/10, 0);
+            update = true;
         }
 
         if (Input.GetKey("x"))
         {
-        	transform.position = transform.position-offsety;
+            transform.Rotate(0, -rotationOffset/10, 0);
+            update = true;
         }
         if (Input.GetKey("q"))
-        {		
-        	transform.Rotate(0,0,-rotationOffset);
+        {               
+                transform.Rotate(0,0,-rotationOffset);
+            update = true;
         }
         if (Input.GetKey("e"))
         {
-        	transform.Rotate(0,0,rotationOffset);
+                transform.Rotate(0,0,rotationOffset);
+            update = true;
         }
-	}
+
+        //update text
+        if (update)
+        {
+            cameraText.text = transform.position.ToString() + transform.rotation.ToString();
+            
+        }
+
+    }
 }
